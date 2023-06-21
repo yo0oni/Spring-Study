f40f950c253dd727534fa5ef6dfb9498b717abd2
Jiyoon Kim <rlawldbs0911@naver.com>
2023-06-21 18:53:48 +0900

    feat: @Transactional 추가

--- src/main/java/com/example/springproject/service/ArticleService.java
+++ src/main/java/com/example/springproject/service/ArticleService.java
@@ -1,2 +1,81 @@
-package com.example.springproject.service;public class ArticleService {
+package com.example.springproject.service;
+
+import com.example.springproject.dto.ArticleForm;
+import com.example.springproject.entity.Article;
+import com.example.springproject.repository.ArticleRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    public Article index(Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+    
+    @Transactional
+    public Article create(ArticleForm dto) {
+        Article article = dto.toEntity();
+
+        if (article.getId() != null) {
+            return null;
+        }
+        return articleRepository.save(article);
+    }
+
+    @Transactional
+    public Article update(Long id, ArticleForm dto) {
+        Article article = dto.toEntity();
+        Article target = articleRepository.findById(id).orElse(null);
+
+        if (target == null || id != article.getId()) {
+            return null;
+        }
+
+        target.patch(article);
+        return articleRepository.save(target);
+    }
+
+    @Transactional
+    public Article delete(Long id) {
+        Article target = articleRepository.findById(id).orElse(null);
+
+        if (target == null) {
+            return null;
+        }
+
+        articleRepository.delete(target);
+        return target;
+    }
+
+    @Transactional
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+
+        // 강제 예외 발생
+        articleRepository.findById(-1L).orElseThrow(
+                () -> new IllegalArgumentException("실패")
+        );
+
+        return articleList;
+    }
 }
