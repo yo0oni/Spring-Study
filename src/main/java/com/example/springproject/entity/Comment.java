2d07f85beb5e5d5da44ca346fa8d3a2c7fa7ff9f
Jiyoon Kim <rlawldbs0911@naver.com>
2023-06-29 03:41:23 +0900

    feat: comment create, patch 예외 코드 작성

--- src/main/java/com/example/springproject/entity/Comment.java
+++ src/main/java/com/example/springproject/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.springproject.entity;
 
+import com.example.springproject.dto.CommentDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,4 +27,31 @@ public class Comment {
 
     @Column
     private String body;
+
+    public static Comment createComment(Article article, CommentDto dto) {
+        if (dto.getId() != null) {
+            throw new IllegalArgumentException("댓글 생성 실패! 댓글의 id가 없어야 합니다.");
+        }
+        if (dto.getArticleId() != article.getId()) {
+            throw new IllegalArgumentException("댓글 생성 실패! 댓글의 id가 잘못 되었습니다.");
+        }
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
+    }
+
+    public void patch(CommentDto dto) {
+        if (this.id != dto.getId()) {
+            throw new IllegalArgumentException("댓글 수정 실패! 잘못된 id가 입력되었습니다.");
+        }
+        if (this.getNickname() != dto.getNickname()) {
+            this.nickname = dto.getNickname();
+        }
+        if (dto.getBody() != null) {
+            this.body = dto.getBody();
+        }
+    }
 }
